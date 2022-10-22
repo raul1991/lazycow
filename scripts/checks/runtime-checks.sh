79a383413f6743e77cd30625b2fbb7469c31d8f0
Valeriyan <32563526+feedbacker1991@users.noreply.github.com>
2022-10-22 23:16:12 +0530

    Synced changes from internal repo to this one (#24)

--- scripts/checks/runtime-checks.sh
+++ scripts/checks/runtime-checks.sh
@@ -2,16 +2,18 @@
 root_dir=$(git rev-parse --show-toplevel)
 
 function test_help_option() {
-    for script in $root_dir/bin/*;
+    for script in $root_dir/bin/lazycow*;
     do
         echo "[simulate] Testing script $script"
         echo "Running with no argument, expecting help menus"
-        source $script 1>&2 >error.txt
+        source $script 1>&2 >lazycow.test.log
         if [[ $? -ne 0 ]];then
-            echo "Tests failed" | lolcat
+            echo "Tests failed" | cowsay
             cat error.txt
             rm -f error.txt
             echo 1
+        else
+            echo "Tests passed for $script"
         fi
     done
 }
