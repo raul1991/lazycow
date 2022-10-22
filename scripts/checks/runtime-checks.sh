26b62559f3b4bea455dbbd27845c1f8baa35114d
Valeriyan <32563526+feedbacker1991@users.noreply.github.com>
2022-10-22 23:38:57 +0530

    fix(cleanup): Clean up bitbucket files and redudant logs (#27)

--- scripts/checks/runtime-checks.sh
+++ scripts/checks/runtime-checks.sh
@@ -4,8 +4,7 @@ root_dir=$(git rev-parse --show-toplevel)
 function test_help_option() {
     for script in $root_dir/bin/lazycow*;
     do
-        echo "[simulate] Testing script $script"
-        echo "Running with no argument, expecting help menus"
+        echo "Running $script with no argument, expecting help menus"
         source $script 1>&2 >lazycow.test.log
         if [[ $? -ne 0 ]];then
             echo "Tests failed" | cowsay
